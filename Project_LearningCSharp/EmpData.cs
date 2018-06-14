5c13a602003ecb066bcc937f4ed022d2bb8c1a9a
vipul <vipul@ASH-NUC-05>
2018-06-14 11:00:00 +0530

    Add project files.

---
+++ Project_LearningCSharp/EmpData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_LearningCSharp
+{
+    class EmpData
+    {
+        Dictionary<int, int> hash = new Dictionary<int, int>();
+
+        public void empNum(int size)
+        {
+            int[] a = new int[size];
+            foreach (int s in a)
+            {
+                Console.WriteLine("Enter Employee Number");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Employee Salary");
+                int sal = int.Parse(Console.ReadLine());
+                hash.Add(num, sal);
+            }
+
+
+            Console.WriteLine("HIGHEST SALARY EMPLOYEE DATA");
+            int max = hash.Values.Max();
+            int key = hash.FirstOrDefault(x => x.Value == hash.Values.Max()).Key;
+            Console.WriteLine("The EMP Number is : "+ key + "     " +  "The maximum salary is: " + max);
+            Console.Read();
+
+            //foreach(KeyValuePair<int, int> pair in hash)
+            //{
+            //    //  Console.WriteLine("Emp NUm: " + pair.Key);
+            //    //  Console.WriteLine("Emp Sal: " + pair.Value);   
+            //}
+          //  Console.Read();
+
+        }
+        
+        
+
+
+
+
+
+    }
+}
---
+++ Project_LearningCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpData empD = new EmpData();
+            int empSize = 5;
+            empD.empNum(empSize);
+        }
+    }
+}
